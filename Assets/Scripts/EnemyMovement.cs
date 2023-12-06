5f0fa0b0c20dd0f7166a384e40c68196eada5213
jmartiniii <135464675+jmartiniii@users.noreply.github.com>
2023-12-05 18:36:01 -0600

    more tweaks

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -12,12 +12,19 @@ public class EnemyMovement : MonoBehaviour
     private float changeTime = 2.0f;
     float newMove;
 
+    public SpriteRenderer rend;
+    public Color newColor;
+    public Color oldColor;
+
     int moveDir = 1;
     // Start is called before the first frame update
     void Start()
     {
+        rend = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        oldColor = rend.color;
+        newColor = Color.red;
     }
 
     // Update is called once per frame
@@ -44,10 +51,12 @@ public class EnemyMovement : MonoBehaviour
 
         if (distance < 4)
         {
+            rend.color = newColor;
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, 2 * speed * Time.deltaTime);
         }
         else
         {
+            rend.color = oldColor;
             newMove = speed * moveDir * Time.deltaTime;
             transform.position = new Vector3(transform.position.x + newMove, transform.position.y);
         }
